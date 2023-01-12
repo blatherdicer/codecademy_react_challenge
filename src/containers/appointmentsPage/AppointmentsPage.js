6bf7559ccdddcb6e668af062faaac21fe34318d1
Bret Richdale <bret@instance.co.nz>
2023-01-12 21:19:24 +1300

    Fix deploy warnings

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { TileList } from "../../components/tileList/TileList";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 
